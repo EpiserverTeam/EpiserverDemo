782001bd1a124f441e0a5414ec4e5f17fec89233
TienTm <manhtien12121995@gmail.com>
2017-07-10 15:36:23 +0700

    Add comment block

---
+++ Models/Blocks/CommentBlock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Forms.Core.Models;
+using EpiserverDemo.Store;
+ 
+
+namespace EpiserverDemo.Models.Blocks
+{
+    [ContentType(DisplayName = "CommentBlock", GUID = "d3d478af-8abf-4cda-a161-10f1076edc3d", Description = "")]
+    public class CommentBlock : BlockData
+    {
+        public virtual List<Submission> AllComment
+
+        {
+            get { return CommentStore.GetComment(); }
+        }
+
+        public bool CompareIDPageWithComment(string idPageString, int idPage)
+        {
+            var contentIDString = new ContentReference(idPageString);
+            var contentIDInt = new ContentReference(idPage.ToString());
+
+            return contentIDString.CompareToIgnoreWorkID(contentIDInt);
+        }
+
+    }
+}
